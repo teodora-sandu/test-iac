b093f0f10cc95adb342828abee0168fe56053fac
Ilianna Papastefanou <6989529+ipapast@users.noreply.github.com>
2021-07-09 16:36:08 +0100

    Update with a banana.

--- test1.tf
+++ test1.tf
@@ -1,5 +1,5 @@
 resource "azurerm_app_service" "allowed" {
-  name                = "example-app-service"
+  name                = "example-banana-service"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = "id-xxxx"
@@ -7,7 +7,7 @@ resource "azurerm_app_service" "allowed" {
 }
 
 resource "azurerm_app_service" "allowed_2" {
-  name                = "example-app-service"
+  name                = "example-banana-service"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = "id-xxx"
